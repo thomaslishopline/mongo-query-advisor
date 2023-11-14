2efa35e2c12f883a5c7f0987ff8daaddfbaf44e6
Thomas Li <thomas.li@shoplineapp.com>
2023-11-14 19:46:56 +0800

    init commit

---
+++ lib/mongo_query_advisor.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mongo_query_advisor/version"
+
+module MongoQueryAdvisor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mongo_query_advisor/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MongoQueryAdvisor
+  VERSION = "0.1.0"
+end
